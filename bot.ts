da116b13e72a13e276b068803294239b02ed5e4b
InsanEagle <insaneachiever@gmail.com>
2025-07-21 16:41:39 +0300

    Configured ESLint and Prettier

--- bot.ts
+++ bot.ts
@@ -1,12 +1,12 @@
 import { Bot } from "grammy";
-import "dotenv/config"
+import "dotenv/config";
 
-const key = process.env.TELEGRAM_BOT_API_KEY
+const key = process.env.TELEGRAM_BOT_API_KEY;
 if (!key) {
-    throw new Error("Cannot find TELEGRAM_BOT_API_KEY")
+  throw new Error("Cannot find TELEGRAM_BOT_API_KEY");
 }
 
-const bot = new Bot(key); 
+const bot = new Bot(key);
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
