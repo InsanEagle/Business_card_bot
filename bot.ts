553ab788137b9c8021726f42a9941c1b711a05e6
InsanEagle <insaneachiever@gmail.com>
2025-07-21 17:47:12 +0300

    Completed bot

--- bot.ts
+++ bot.ts
@@ -1,4 +1,4 @@
-import { Bot } from "grammy";
+import { Bot, Keyboard, InlineKeyboard } from "grammy";
 import "dotenv/config";
 
 const key = process.env.TELEGRAM_BOT_API_KEY;
@@ -8,7 +8,39 @@ if (!key) {
 
 const bot = new Bot(key);
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+const keyboard = new Keyboard()
+  .text("Обо мне")
+  .text("Мои услуги")
+  .text("Контакты");
+
+const inlineContactsKeyboard = new InlineKeyboard()
+  .url("Мой GitHub", "https://github.com/InsanEagle")
+  .url("Мой fl.ru", "fl.ru/users/insaneachiever");
+
+bot.command("start", (ctx) => {
+  ctx.reply(
+    "Welcome. You currently using my business card bot. There you can see information about me.",
+    { reply_markup: keyboard }
+  );
+});
+
+bot
+  .on("message:text")
+  .hears("Обо мне", (ctx) =>
+    ctx.reply(
+      "Меня зовут Шамиль, 24 года. Занимаюсь изучением программирования и планирую создавать Телеграм ботов для фриланса. Данный бот - первый проект, который я создал с нуля."
+    )
+  );
+bot
+  .on("message:text")
+  .hears("Мои услуги", (ctx) =>
+    ctx.reply("Могу создать простейшего бота без сложной бизнес-логики")
+  );
+bot.on("message:text").hears("Контакты", (ctx) =>
+  ctx.reply("Ссылки на мои профили:", {
+    reply_markup: inlineContactsKeyboard,
+  })
+);
 
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
