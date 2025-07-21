02ee8ede42c4755301e398e2068c6c14b7692b8b
InsanEagle <insaneachiever@gmail.com>
2025-07-21 15:30:25 +0300

    Created bot core

---
+++ bot.ts
@@ -0,0 +1,15 @@
+import { Bot } from "grammy";
+import "dotenv/config"
+
+const key = process.env.TELEGRAM_BOT_API_KEY
+if (!key) {
+    throw new Error("Cannot find TELEGRAM_BOT_API_KEY")
+}
+
+const bot = new Bot(key); 
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+bot.start();
